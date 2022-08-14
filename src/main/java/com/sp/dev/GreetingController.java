fb2d53aa92384a760910a8f5d6bfe2c7674a54a7
prabhu4029 <prabhu4tx@gmail.com>
2022-08-14 01:27:06 -0500

    added changes

--- src/main/java/com/sp/dev/GreetingController.java
+++ src/main/java/com/sp/dev/GreetingController.java
@@ -1,3 +1,7 @@
+package com.sp.dev;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
