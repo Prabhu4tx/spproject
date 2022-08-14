e2796b8f74d0a76a561842fd22b5e0dc76a58cdf
prabhu4029 <prabhu4tx@gmail.com>
2022-08-14 01:32:53 -0500

    added changes in Application.java

--- src/main/java/com/sp/dev/Application.java
+++ src/main/java/com/sp/dev/Application.java
@@ -1,3 +1,8 @@
+
+package com.sp.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
